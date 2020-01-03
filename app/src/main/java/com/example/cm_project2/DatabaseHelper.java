4468beaa946c0bff79cfa2cb430629b9f123b746
mcorreia <mascorreia@users.noreply.github.com>
2020-01-03 15:55:56 +0000

    Database in progress

---
+++ app/src/main/java/com/example/cm_project2/DatabaseHelper.java
@@ -0,0 +1,31 @@
+package com.example.cm_project2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "LuBan.db";
+    public static final String TABLE_NAME = "fourth_station";
+    public static final String COL_1 = "ID";
+
+
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
