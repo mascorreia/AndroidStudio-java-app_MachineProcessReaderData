948bafc3b155420f3b07e0727a139492ea50df07
Rafael <180221092@estudantes.ips.pt>
2020-01-11 15:48:37 +0000

    Splash activity and image

--- app/src/main/java/com/example/cm_project2/SplashActivity.java
+++ app/src/main/java/com/example/cm_project2/SplashActivity.java
@@ -1,17 +1,59 @@
 package com.example.cm_project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.netopen.hotbitmapgg.library.view.RingProgressBar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
+
+    RingProgressBar ringProgressBar1;
+
+    int progress = 0;
+
+    Handler myHandler  = new Handler(){
+
+        @Override
+        public void handleMessage(Message msg){
+
+            if(msg.what == 0)
+            {
+                if(progress < 100)
+                {
+                    progress++;
+                    ringProgressBar1.setProgress(progress);
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ringProgressBar1 = (RingProgressBar) findViewById(R.id.progress_bar_1);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 100; i++){
+
+                    try {
+                        Thread.sleep(100);
+                        myHandler.sendEmptyMessage(0);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+
+                    }
+                }
+            }
+        }).start();
+
         new Handler().postDelayed(new Runnable() {
 
             @Override
@@ -22,6 +64,6 @@ public class SplashActivity extends AppCompatActivity {
 
                 finish();
             }
-        }, 4000);
+        }, 10000);
     }
 }
